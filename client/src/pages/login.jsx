23149469cda771e9407e4d212e41fe97361667d4
Tommy <105886743+ChristopherTommy0506@users.noreply.github.com>
2024-02-19 09:27:59 -0600

    Create login.jsx

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function login() {
+     return(
+        <>
+        <header>
+            <div id="header">
+            <div className="bg-sky-500"></div>
+            <div className="bg-yellow-500"></div>
+            </div>
+        </header>
+        </>
+     )
+}
