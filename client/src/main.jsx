be8de7f17948f653eb29af6f320e7904d1728c61
Tommy <105886743+ChristopherTommy0506@users.noreply.github.com>
2024-02-20 11:34:37 -0600

    Añadí las primeras pantallas form/login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import {App} from './App.jsx'
+import { Login } from './pages/login.jsx';
+import { Form } from './pages/form.jsx';
 import "./index.css";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 
@@ -9,11 +11,28 @@ const router = createBrowserRouter([
     path: "/",
     element: (
       <>
-      <App/>
+      <App />
       </>
     ),
   },
+{
+  path: "/login",
+  element: (
+    <>
+      <Login />
+    </>
+  )
+},
+{
+path: "/form",
+element: (
+  <>
+    <Form />
+  </>
+)
+}
 ]);
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import donbosco from "../assets/donbosco.png";
+import batu from "../assets/batu.png";
+import inicio3 from "../assets/inicio3.jpg";
 
-export function login() {
-     return(
+export function Login() {
+    return (
         <>
-        <header>
-            <div id="header">
-            <div className="bg-sky-500"></div>
-            <div className="bg-yellow-500"></div>
-            </div>
-        </header>
+            <body>
+                <main className="grid grid-cols-2 row-auto
+">
+                    <section className="h-screen">
+                        <img className="w-[900px] h-1/2 " src={batu} alt="" />
+                        <img className="w-[900px] h-1/2 " src={inicio3} alt="" />
+
+
+                    </section>
+                    <section className="font-bold text-2xl justify-center items-center flex flex-col">
+                        <h1>"El sueño que hace soñar"</h1>
+                        <h1 className="pt-[40px]">Un corazón que tranforma "lobos" en "corderos"</h1>
+                        <a href=""><button className=" rounded-xl bg-black radius w-56 m-24">
+                            <p className="p-3 text-white text-sm">Iniciar</p>
+                        </button></a>
+                    </section>
+                </main>
+            </body>
         </>
-     )
+    )
 }
