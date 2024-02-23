0d15e90968939ff103d85459bb2a11071940c3f5
XavierXZCH <xavi042005@gmail.com>
2024-02-22 18:54:46 -0600

    Añadidura de una imagen y hecho responsive

--- client/src/pages/log_cat.jsx
+++ client/src/pages/log_cat.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import bgfondo from "../assets/bg-form.jpg"
 export function Log_cat() {
     return(
         <body>
-            <main className="grid justify-items-center p-2 ">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+            <img className="max-w-full h-auto p-4 m-2 md:m-12" src={bgfondo} alt="" />
+            <main className="grid justify-items-center p-2">
                 <div className="flex gap-6 text-center border-solid border-2 border-amber-300 w-full md:w-96 p-2 m-6">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
@@ -44,6 +47,9 @@ export function Log_cat() {
                                 </div>
                                     </section>
                                     </main>
+
+            </div>
                                     </body>
+                                    
     )
 }
