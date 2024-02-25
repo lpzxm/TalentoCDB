e4c7d06af8e28ec04089981c1675128493b20b53
DMurgas <105890876+DMurgas@users.noreply.github.com>
2024-02-25 00:19:22 -0600

    Olvidaba agregar esto

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,9 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Welcome } from './pages/Welcome/welcome.jsx';
+import { Registrar_Rubrica } from './pages/Registrar_Rubrica/Registrar_Rubrica.jsx';
+import { N_campo } from './pages/N_campo/N_campo.jsx';
+import { Tabla_Resultados } from './pages/Tabla_Resultados/Tabla_Resultados.jsx';
 import { Login } from './pages/Login/login.jsx';
 import { Log_cat } from './pages/RegistrarCategoria/log_cat.jsx';
 import {Home} from './pages/home.jsx';
