a6ab43893735fb66713af62d87a9b44e322f133f
lpzxm <ja044840@gmail.com>
2024-02-25 21:10:52 -0600

    Update Header.jsx

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,10 +9,23 @@ export function Header() {
             <header className='h-32 md:visible md:relative fixed flex flex-col'>
                 <div className='bg-yellow-400 h-full'></div>
                 <div className='bg-blue-700 h-full'></div>
-                <div className="bg-white px-2 py-1 absolute top-2 w-full h-fit">
-                    <img className='w-12 h-fit' src={donbo} alt="DB" />
-                    <p>hola</p>
+                <div className="absolute top-3 w-full px-3 ">
+                    <div className="w-full h-fit px-6 py-3 backdrop-blur-sm bg-white/80 flex-col md:flex-row">
+                        <img className='w-12 h-fit' src={donbo} alt="DB" />
+                        <ul className="flex flex-col md:flex-row p-4">
+                            <li>
+                                Home
+                            </li>
+                            <li>
+                                Registrar Categoria
+                            </li>
+                            <li>
+                                Ver categorias
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+
             </header>
         </>
     )
