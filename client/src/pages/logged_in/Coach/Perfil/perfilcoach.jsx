8dc8409f7cd6ad9bfaf8937069df41ae34ea6179
lpzxm <88464727+lpzxm@users.noreply.github.com>
2024-08-21 10:15:11 -0600

    Update perfilcoach.jsx

--- client/src/pages/logged_in/Coach/Perfil/perfilcoach.jsx
+++ client/src/pages/logged_in/Coach/Perfil/perfilcoach.jsx
@@ -25,7 +25,7 @@ export const PerfilCoach = () => {
 
         </div>
         <p className="mb-6">Descripción del Coach:</p>
-        <CoachDetail iconName={<CiUser size={"30px"} color='' />} />
+        <CoachDetail iconName={<CiUser size={"30px"} color='' />} text={usuario.descripcion} />
       </section>
     </article>
   );
