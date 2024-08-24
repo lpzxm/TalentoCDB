d8870f453a00e2f14a54312c409a872dbc880ae2
Tommy <christophernunez641@gmail.com>
2024-08-24 17:40:35 -0600

    Translate
    Se optimizó el responsive del translate

---
+++ client/src/components/ui/translate/mobiletranslate.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { MdOutlineTranslate } from "react-icons/md";
+
+const TranslateMobi = () => {
+    const [isLanguageSelectorVisible, setIsLanguageSelectorVisible] = useState(false);
+
+    const googleTranslateElementInit = () => {
+        if (!window.google?.translate?.TranslateElement) return;
+        new window.google.translate.TranslateElement(
+            {
+                autoDisplay: false,
+                includedLanguages: 'en,es,de,ja,fr,it,pt,zh-CN,hi,ru,el,no',
+
+            },
+            "google_translate_element"
+        );
+    };
+
+    useEffect(() => {
+        if (!document.getElementById('google_translate_element')) {
+            const translateDiv = document.createElement('div');
+            translateDiv.classList.add(
+                'z-50',
+                'bottom-0',
+                'bg-white',
+                'right-0',
+                'w-fit',
+                'fixed',
+                'transition-all',
+                'duration-300',
+                'opacity-0',
+                'invisible'
+            );
+            translateDiv.id = 'google_translate_element';
+            document.getElementById('root').appendChild(translateDiv);
+
+            if (!document.querySelector("[src*='translate.google.com/translate_a/element.js']")) {
+                const addScript = document.createElement("script");
+                addScript.setAttribute(
+                    "src",
+                    "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+                );
+                document.body.appendChild(addScript);
+                window.googleTranslateElementInit = googleTranslateElementInit;
+            } else {
+                googleTranslateElementInit();
+            }
+        }
+    }, []);
+
+    const toggleLanguageSelector = () => {
+        const translateElement = document.getElementById('google_translate_element');
+        setIsLanguageSelectorVisible(!isLanguageSelectorVisible);
+        if (isLanguageSelectorVisible) {
+            translateElement.classList.add('opacity-0', 'invisible');
+            translateElement.classList.remove('opacity-100', 'visible');
+        } else {
+            translateElement.classList.add('opacity-100', 'visible');
+            translateElement.classList.remove('opacity-0', 'invisible');
+        }
+    };
+
+    return (
+        <div className="fixed bottom-20 right-0 z-50 p-4">
+
+            <button
+            
+                onClick={toggleLanguageSelector}
+                className="bg-blue-500 p-4 rounded-full hover:bg-blue-800"
+                
+            >
+                <MdOutlineTranslate/>
+            </button>
+        </div>
+    );
+};
+
+export default TranslateMobi;
--- client/src/components/ui/translate/translate.jsx
+++ client/src/components/ui/translate/translate.jsx
@@ -1,74 +1,30 @@
 import { useEffect, useState } from "react";
+import TranslateDesk from "./dektoptranslate"; // Cambiado aquí
+import TranslateMobi from "./mobiletranslate"; // Asegúrate de que esta ruta sea correcta
 
-const Translate = () => {
-    const [isLanguageSelectorVisible, setIsLanguageSelectorVisible] = useState(false);
-
-    const googleTranslateElementInit = () => {
-        if (!window.google?.translate?.TranslateElement) return;
-        new window.google.translate.TranslateElement(
-            {
-                autoDisplay: false,
-                includedLanguages: 'en,es,de,ja,fr,it,pt,zh-CN,hi,ru,el,no',
-
-            },
-            "google_translate_element"
-        );
-    };
+// Componente principal que se exporta por defecto
+function Translate() {
+    const [isDesktop, setIsDesktop] = useState(false);
 
     useEffect(() => {
-        if (!document.getElementById('google_translate_element')) {
-            const translateDiv = document.createElement('div');
-            translateDiv.classList.add(
-                'z-50',
-                'bottom-14',
-                'bg-white',
-                'right-0',
-                'w-fit',
-                'fixed',
-                'transition-all',
-                'duration-300',
-                'opacity-0',
-                'invisible'
-            );
-            translateDiv.id = 'google_translate_element';
-            document.getElementById('root').appendChild(translateDiv);
+        const manageSize = () => {
+            setIsDesktop(window.innerWidth >= 900);
+        };
 
-            if (!document.querySelector("[src*='translate.google.com/translate_a/element.js']")) {
-                const addScript = document.createElement("script");
-                addScript.setAttribute(
-                    "src",
-                    "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-                );
-                document.body.appendChild(addScript);
-                window.googleTranslateElementInit = googleTranslateElementInit;
-            } else {
-                googleTranslateElementInit();
-            }
-        }
+        manageSize();
+
+        window.addEventListener("resize", manageSize);
+
+        return () => {
+            window.removeEventListener("resize", manageSize);
+        };
     }, []);
 
-    const toggleLanguageSelector = () => {
-        const translateElement = document.getElementById('google_translate_element');
-        setIsLanguageSelectorVisible(!isLanguageSelectorVisible);
-        if (isLanguageSelectorVisible) {
-            translateElement.classList.add('opacity-0', 'invisible');
-            translateElement.classList.remove('opacity-100', 'visible');
-        } else {
-            translateElement.classList.add('opacity-100', 'visible');
-            translateElement.classList.remove('opacity-0', 'invisible');
-        }
-    };
-
     return (
-        <div className="fixed bottom-0 right-10 z-50 p-4">
-            <button
-                className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition duration-300"
-                onClick={toggleLanguageSelector}
-            >
-                Seleccionar idioma
-            </button>
-        </div>
+        <>
+            {isDesktop ? <TranslateDesk /> : <TranslateMobi />}
+        </>
     );
-};
+}
 
 export default Translate;
